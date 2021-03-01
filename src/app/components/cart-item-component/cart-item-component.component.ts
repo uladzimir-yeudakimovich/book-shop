c428a6ac52416d51e36242f9ea243e23d96fb5e4
ULADZIMIR YEUDAKIMOVICH <uladzimir.yeudakimovich@gmail.com>
2021-03-02 02:14:57 +0300

    fix: delete onpush

--- src/app/components/cart-item-component/cart-item-component.component.ts
+++ src/app/components/cart-item-component/cart-item-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 import { ICart } from '../../models/CartModel';
 
@@ -6,7 +6,6 @@ import { ICart } from '../../models/CartModel';
   selector: 'app-cart-item-component',
   templateUrl: './cart-item-component.component.html',
   styleUrls: ['./cart-item-component.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CartItemComponentComponent {
   @Input() books: ICart[] = [];
